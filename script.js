c40dee515b3ea5b829bb8d6263ad07a646c77eb7
岩崎百花 <iwasakimomoka@iwasakihyakkanoMacBook-Air.local>
2020-08-24 10:33:51 +0900

    Branch2 push

---
+++ script.js
@@ -0,0 +1,61 @@
+window.onload = function () {
+    scroll_effect();
+
+    $(window).scroll(function () {
+        scroll_effect();
+    });
+
+    function scroll_effect() {
+        $('.effect-fade').each(function () {
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 80) {
+                $(this).addClass('effect-scroll');
+            }
+        });
+    }
+};
+
+
+
+$(function () {
+    $(window).scroll(function () {
+        $('.effect-fade').each(function () {
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 80) {
+                $(this).addClass('effect-scroll');
+            }
+        });
+    });
+});
+
+$(function () {
+    $(window).scroll(function () {
+        $('.effect-fade1').each(function () {
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 80) {
+                $(this).addClass('effect-scroll');
+            }
+        });
+    });
+});
+
+$(function () {
+    $(window).scroll(function () {
+        $('.effect-fade2').each(function () {
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 80) {
+                $(this).addClass('effect-scroll');
+            }
+        });
+    });
+});
+
+
